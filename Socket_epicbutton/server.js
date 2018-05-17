d14ddf4f765b10115ffb70f553ce9415d0f6a39e
surrock <erin.surrock@gmail.com>
2018-05-16 23:51:49 -0400

    Sockets Epic Button Game

--- Socket_epicbutton/server.js
+++ Socket_epicbutton/server.js
@@ -1,52 +1,48 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
 
 var app = express();
-app.use(express.static(path.join(__dirname, './static')));
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "./static")));
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
 
-
-app.get('/', function(request, response) {
-	response.render('index');
-
+app.get("/", function(request, response) {
+  response.render("index");
 });
 
 var server = app.listen(8000, function() {
-	console.log('Server is listening on port 8000');
+  console.log("Server is listening on port 8000");
 });
-var io = require('socket.io').listen(server);
+var io = require("socket.io").listen(server);
 
-io.sockets.on('connection', function(socket) {
+io.sockets.on("connection", function(socket) {
+  function update() {
+    io.emit("update", { count: count });
+  }
 
-	function update() {
-		io.emit('update', {count:count});
-	}
+  socket.on("start", function() {
+    if (typeof count != "number") {
+      count = 0;
+    }
+    update();
+    console.log(io.engine.clientsCount);
+  });
 
-	socket.on('start', function() {
-		if (typeof count != 'number') {
-			count = 0;
-		}
-		update();
-		console.log(io.engine.clientsCount);
-	});
+  socket.on("epic", function() {
+    count += 1;
+    update();
+  });
+  socket.on("bold", function() {
+    count += 2;
+    update();
+  });
+  socket.on("cold", function() {
+    count -= 1;
+    update();
+  });
 
-	socket.on('epic', function() {
-		count += 1;
-		update();
-	});
-	socket.on('bold', function() {
-		count += 2;
-		update();
-	});
-	socket.on('cold', function() {
-		count -= 1;
-		update();
-	});
-
-	socket.on('reset', function() {
-		count = 0;
-		update();
-	});
-
+  socket.on("reset", function() {
+    count = 0;
+    update();
+  });
 });
