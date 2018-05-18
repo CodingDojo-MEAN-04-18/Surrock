ba2b5fb96982f4d4abb7a9b451865f89cdf7733a
surrock <erin.surrock@gmail.com>
2018-05-18 00:18:01 -0400

    Socket Colors and fixing typos

--- Socket_epicbutton/server.js
+++ Socket_epicbutton/server.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var path = require("path");
-
+var bodyParser = require('body-parser');
+var session = require('express-session');
+app.use(session({secret: 'shhhItsasecret'}));
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, "./static")));
-app.set("views", path.join(__dirname, "./views"));
-app.set("view engine", "ejs");
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
 
 app.get("/", function(request, response) {
   response.render("index");
--- Socket_surveyRevisited/server.js
+++ Socket_surveyRevisited/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({secret: 'codingdojorocks'}));
+app.use(session({secret: 'shhhItsasecret'}));
 app.use(express.static(path.join(__dirname, "./static")));
 
 app.set('views', path.join(__dirname, './views'));
