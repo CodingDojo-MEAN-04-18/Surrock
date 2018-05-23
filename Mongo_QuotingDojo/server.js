c61b9e1ce7326415abf4a4a924b40f3c835f310a
surrock <erin.surrock@gmail.com>
2018-05-23 00:00:58 -0400

    Mongo Login and Registration

--- Mongo_QuotingDojo/server.js
+++ Mongo_QuotingDojo/server.js
@@ -1,47 +1,54 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var mongoose = require('mongoose');
-var path = require('path');
-var bodyParser = require('body-parser');
-var session = require('express-session');
+var mongoose = require("mongoose");
+var path = require("path");
+var bodyParser = require("body-parser");
+var session = require("express-session");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, './static')));
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
-app.use(session({secret: 'shhhhThisisAsecret!'}));
+app.use(express.static(path.join(__dirname, "./static")));
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+app.use(session({ secret: "shhhhThisisAsecret!" }));
 
+mongoose.connect("mongodb://localhost/quotes");
+var QuoteSchema = new mongoose.Schema(
+  {
+    name: { type: String },
+    quote: { type: String },
+    author: { type: String }
+  },
+  { timestamps: true }
+);
 
-mongoose.connect('mongodb://localhost/quotes');
-var QuoteSchema = new mongoose.Schema({
-	name: {type: String},
-	quote: {type: String},
-author: {type: String}
-}, {timestamps: true});
-mongoose.model('Quote', QuoteSchema); 
-var Quote = mongoose.model('Quote') 
+mongoose.model("Quote", QuoteSchema);
+var Quote = mongoose.model("Quote");
 mongoose.Promise = global.Promise;
 
-
-
-app.get('/', function(req, res) {
-    res.render('index');
-})
-
-app.get('/quotes', function(req, res) {
-    Quote.find({}, function(err, quotes) {
-        if (err) { console.log(err); }
-        else { res.render('quotes', { quotes: quotes }); }
-      });
+app.get("/", function(req, res) {
+  res.render("index");
 });
 
-app.post('/quotes', function(req, res) {
-    Quote.create(req.body, function(err) {
-        if (err) { console.log(err); }
-        else { res.redirect('/quotes'); }
-    });
+app.get("/quotes", function(req, res) {
+  Quote.find({}, function(err, quotes) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("quotes", { quotes: quotes });
+    }
+  });
+});
+
+app.post("/quotes", function(req, res) {
+  Quote.create(req.body, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/quotes");
+    }
+  });
 });
 
 app.listen(8000, function() {
-    console.log("Listening on port 8000");
+  console.log("Listening on port 8000");
 });
